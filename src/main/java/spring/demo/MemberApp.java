7e3081e136e90f7e509811655af4c4a2a70ba4ea
woojin126 <woojin126@naver.com>
2021-04-10 14:53:13 +0900

    실험 App

---
+++ src/main/java/spring/demo/MemberApp.java
@@ -0,0 +1,17 @@
+package spring.demo;
+
+import spring.demo.Member.Grade;
+import spring.demo.Member.Member;
+import spring.demo.service.MemberService;
+import spring.demo.service.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
