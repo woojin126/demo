eda7937086e6b29e81dd7f6ff7d9a8ed73f6f0fc
woojin126 <woojin126@naver.com>
2021-04-11 08:39:15 +0900

    Member 서비스

--- src/main/java/spring/demo/service/MemberServiceImpl.java
+++ src/main/java/spring/demo/service/MemberServiceImpl.java
@@ -7,7 +7,7 @@ import spring.demo.repository.MemberRepositoryImpl;
 public class MemberServiceImpl implements MemberService{
 
     /*이곳을보면 의존관계가 인터페이스뿐만 아니라 구현까지 의존해버림 */
-   MemberRepository memberRepository = new MemberRepositoryImpl();
+    private final MemberRepository memberRepository = new MemberRepositoryImpl();
     @Override
     public void join(Member member) {
         memberRepository.save(member);
