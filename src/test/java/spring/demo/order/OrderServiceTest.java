a02ef76abe8aab831b70c7874ff239c3ee4edfea
woojin126 <woojin126@naver.com>
2021-04-11 11:57:49 +0900

    OCP,DIP 를 다 지킬수 있는 의존방식 수정 코드 (테스트)

--- src/test/java/spring/demo/order/OrderServiceTest.java
+++ src/test/java/spring/demo/order/OrderServiceTest.java
@@ -1,7 +1,9 @@
 package spring.demo.order;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.demo.AppConfig;
 import spring.demo.Member.Grade;
 import spring.demo.Member.Member;
 import spring.demo.service.MemberService;
@@ -11,8 +13,18 @@ import spring.demo.service.OrderServiceImpl;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    //MemberService memberService = new MemberServiceImpl();
+    //OrderService orderService = new OrderServiceImpl();
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig=new AppConfig();
+       memberService =  appConfig.memberService();
+       orderService =  appConfig.orderService();
+    }
 
     @Test
     void serviceTest() {
