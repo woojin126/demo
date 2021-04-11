729f6cd5add60b513140d8dff4e9b299edd4cb34
woojin126 <woojin126@naver.com>
2021-04-11 11:57:29 +0900

    OCP,DIP 를 다 지킬수 있는 의존방식 수정 코드 (생성자 이용)

--- src/main/java/spring/demo/service/OrderServiceImpl.java
+++ src/main/java/spring/demo/service/OrderServiceImpl.java
@@ -10,9 +10,9 @@ import spring.demo.repository.MemberRepositoryImpl;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new
+  /*  private final MemberRepository memberRepository = new
             MemberRepositoryImpl();
-    /*1.역할과 구현을 분리함
+  */  /*1.역할과 구현을 분리함
     * 2.다형성 활용 ok, 인터페이스 구현객체 분리 ok
     * OCP,DIP 같은 객체지향 설계를 적용한거처럼 보이지만 X
     * DIP->
@@ -21,8 +21,16 @@ public class OrderServiceImpl implements OrderService{
     * 이걸보면 지금 기능 확장해서 변경할시 클라이언트 코드에 영향을준다. 따라서 OCP 위반
     * */
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /*OCP ,DIP 가지켜지도록 수정해보자*/
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice)
     {
