32466e04c52a95542e0cd14b1b0c79b324329d8a
woojin126 <woojin126@naver.com>
2021-04-11 11:57:02 +0900

    OCP,DIP 를 다 지킬수 있는 의존방식 수정 코드 (테스트)

--- src/main/java/spring/demo/OrderApp.java
+++ src/main/java/spring/demo/OrderApp.java
@@ -11,13 +11,25 @@ import spring.demo.service.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+       /* MemberService memberService = new MemberServiceImpl();
         OrderService orderService = new OrderServiceImpl();
         long memberId = 1L;
 
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
-        System.out.println("order = " + order);
+        System.out.println("order = " + order);*/
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        Member member = new Member(1L,"woojin",Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(1L, "itemA,", 10000);
+        System.out.println("order =" + order);
+
+
     }
 }
