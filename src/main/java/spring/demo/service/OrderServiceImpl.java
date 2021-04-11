4ce1084c5c15aabb5c88cd9775065c83a3fefa24
woojin126 <woojin126@naver.com>
2021-04-11 10:01:23 +0900

    역할과 구현을 분리함 but 보이는 함정이 있음 이것은지금 OCP 위반한것 주석달아놓음

--- src/main/java/spring/demo/service/OrderServiceImpl.java
+++ src/main/java/spring/demo/service/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package spring.demo.service;
 import spring.demo.Member.Member;
 import spring.demo.discount.DiscountPolicy;
 import spring.demo.discount.FixDiscountPolicy;
+import spring.demo.discount.RateDiscountPolicy;
 import spring.demo.order.Order;
 import spring.demo.repository.MemberRepository;
 import spring.demo.repository.MemberRepositoryImpl;
@@ -11,7 +12,17 @@ public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new
             MemberRepositoryImpl();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /*1.역할과 구현을 분리함
+    * 2.다형성 활용 ok, 인터페이스 구현객체 분리 ok
+    * OCP,DIP 같은 객체지향 설계를 적용한거처럼 보이지만 X
+    * DIP->
+    * 추상의존:DiscountPolicy
+    * 구현체 클래스:FixDiscountPolicy,RateDiscountPolicy
+    * 이걸보면 지금 기능 확장해서 변경할시 클라이언트 코드에 영향을준다. 따라서 OCP 위반
+    * */
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice)
     {
