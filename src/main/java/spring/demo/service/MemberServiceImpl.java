d37b702e7cffd0d03b7091ad301c22e30f18f87c
woojin126 <woojin126@naver.com>
2021-04-11 11:56:30 +0900

    OCP,DIP 를 다 지킬수 있는 의존방식 수정 코드

--- src/main/java/spring/demo/service/MemberServiceImpl.java
+++ src/main/java/spring/demo/service/MemberServiceImpl.java
@@ -7,7 +7,16 @@ import spring.demo.repository.MemberRepositoryImpl;
 public class MemberServiceImpl implements MemberService{
 
     /*이곳을보면 의존관계가 인터페이스뿐만 아니라 구현까지 의존해버림 */
-    private final MemberRepository memberRepository = new MemberRepositoryImpl();
+    //private final MemberRepository memberRepository = new MemberRepositoryImpl();
+    /*OCP,DIP 를 다 지킬수 있는 코드 로 변경
+    * 아래 코드 MemberReServiceImpl는 MemberRepositoryImpl 를 의존하지 않는다
+    * */
+        private final MemberRepository memberRepository;
+
+        public MemberServiceImpl(MemberRepository memberRepository){
+            this.memberRepository=memberRepository;
+        }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
