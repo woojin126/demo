149583ced17864c8d7f79a5454253f463b71454c
woojin126 <woojin126@naver.com>
2021-04-13 11:19:15 +0900

    싱글톤 Prototype 라이프 사이클 테스트

--- src/test/java/spring/demo/singleton/SingletonTest.java
+++ src/test/java/spring/demo/singleton/SingletonTest.java
@@ -3,8 +3,13 @@ package spring.demo.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 import spring.demo.AppConfig;
 import spring.demo.service.MemberService;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 public class SingletonTest {
     
@@ -17,5 +22,32 @@ public class SingletonTest {
 
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
 
+    }
+
+    @Test
+    public void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac =new AnnotationConfigApplicationContext(SingletonBean.class);
+        System.out.println("prototypeBean1");
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        System.out.println("prototypeBean2");
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        Assertions.assertThat(singletonBean1).isNotSameAs(singletonBean2);
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class SingletonBean{
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destory"); //138 에서다시
+        }
     }
 }
