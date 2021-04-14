0b43795fcc37f843a16a4792327fbd6bc301266c
woojin126 <woojin126@naver.com>
2021-04-14 17:50:38 +0900

    MemberApp

--- src/main/java/spring/demo/MemberApp.java
+++ src/main/java/spring/demo/MemberApp.java
@@ -9,16 +9,17 @@ import spring.demo.service.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-      /*  MemberService memberService = new MemberServiceImpl();
+        //MemberService memberService = new MemberServiceImpl();
+       /* AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());*/
 
-        // AppConfig appConfig = new AppConfig();
-        // MemberService memberService = appConfig.memberService();
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+       ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L,"woojin",Grade.VIP);
